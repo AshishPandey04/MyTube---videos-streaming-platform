4515405f372cbbe1518cb0a3cbcdb3104e568238
AshishPandey04 <0203ashish2004@gmail.com>
2024-11-18 15:51:33 +0530

    .

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running at port : ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("MONGO db connection failed !!!",err)
+
+})
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
