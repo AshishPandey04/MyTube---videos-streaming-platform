0a1e2b56dd4ae81558ebc59210fcd940d8b728a9
AshishPandey04 <0203ashish2004@gmail.com>
2025-03-01 18:18:06 +0530

    bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -35,7 +37,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true,"Password is required"],
+        required: [true, "Password is required"],
 
     },
     watchHistory: [
@@ -44,10 +46,25 @@ const userSchema = new mongoose.Schema({
             ref: "Video",
         }
     ],
-    refreshToken:{
-        type:String,
+    refreshToken: {
+        type: String,
     }
 
 }, { timestamps: true })
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+
+    this.password = bcrypt.hash(this.password, 10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect=async function (password) {
+  return await bcrypt.compare(password,this.password)
+}
+
 export const User = mongoose.model("User", userSchema);
+// arrow function ko this ka excess nahi hota , so woh current instance ko nahi de sakta
