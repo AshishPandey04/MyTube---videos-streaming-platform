894c91d1cbd1da7c7706d1d726e4c59729747939
AshishPandey04 <0203ashish2004@gmail.com>
2025-04-10 00:33:56 +0530

    logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
-import { registerUser } from "../controllers/user.controller.js"
+import { loginUser,registerUser, logoutUser } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 
 const router=Router();
@@ -16,7 +17,16 @@ router.route("/register").post(
             maxCount:1
         }
     ]),
-    registerUser)
+    registerUser
+)
+
+router.route("/login").post(loginUser)
+
+
+//secired route
+router.route("/logout").post(verifyJWT,logoutUser)
+
+
 
 
 
