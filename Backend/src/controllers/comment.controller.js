9c1f40ac0c51340bee11f148e15eb8b771e358b6
AshishPandey04 <0203ashish2004@gmail.com>
2025-04-18 00:25:33 +0530

    Enhance comment controller to validate video and comment IDs, improve error handling, and update response structure for fetching comments. Add functionality for adding, updating, and deleting comments with appropriate checks for user authentication and authorization.

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import { Comment } from "../models/comment.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
@@ -13,17 +13,19 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 
 
-    if (!videoId?.trim()) {
+    if (!isValidObjectId(videoId)) {
         throw new ApiError(400, "Invalid videoId")
     }
 
-    const comments = await Comment.aggregate([
+    const videoObjectId = new mongoose.Types.ObjectId(videoId);
+    const commentsData = await Comment.aggregate([
         {
             $match: {
-                video: videoId
+                video: videoObjectId
             }
 
         },
+   
         {
             $lookup: {
                 from: "users",
@@ -33,7 +35,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
                 pipeline: [
                     {
                         $project: {
-                            fullName: 1,
+
                             username: 1,
                             avatar: 1
                         }
@@ -43,36 +45,36 @@ const getVideoComments = asyncHandler(async (req, res) => {
         },
 
         {
-            $unwind: "$user"
+            $unwind: "$owner"
 
         },
+     
         {
             $sort: {
                 createdAt: -1
             }
         },
+
+        { 
+            $skip: (page - 1) * limit 
+        },
         {
-            $facet: {
-                comments: [
-                    { $skip: (page - 1) * limit },
-                    { $limit: limit }
-                ],
-                totalCount: [
-                    { $count: "count" }
-                ]
-            }
+             $limit: limit 
         }
 
 
     ])
-    const totalPages = Math.ceil(totalCount / limit)
+    if (!commentsData?.length) {
+        throw new ApiError(404, "Comments are not found");
+    }
+
 
 
 
     return res
         .status(200)
         .json(
-            new ApiResponse(200, { comments, totalCount, totalPages }, "Comments fetched successfully")
+            new ApiResponse(200, { commentsData }, "Comments fetched successfully")
         )
 
 
@@ -80,16 +82,101 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const { content } = req.body;
+    const { videoId } = req.params;
 
-    
+    if (!content) {
+        throw new ApiError(400, "Comment is required , Cannot be empty")
+    }
+
+    if (!videoId) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+    if (!req.user) {
+        throw new ApiError(400, "User must be loggedin")
+    }
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req.user?._id
+    })
+
+    return res.status(201).json(
+        new ApiResponse(201, comment, "Comment added successfully")
+    )
+
+
+
+
+
+
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const { content } = req.body;
+    const { commentId } = req.params
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "Comment cannot be empty or just spaces")
+    }
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+    }
+
+
+    if (!req.user) {
+        throw new ApiError(401, "User must be logged in");
+    }
+
+
+    const updatedComment = await Comment.findOneAndUpdate(
+        {
+            _id: commentId,
+            owner: req.user?._id,
+        },
+        {
+            $set: {
+                content,
+            },
+        },
+        { new: true }
+    );
+    if (!updatedComment) {
+        throw new ApiError(404, "Comment not found or you're not authorized to update it");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedComment, "Comment successfully updated"));
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+
+    const { commentId } = req.params
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "INVALID COMMENT ID ")
+    }
+
+    if (!req.user) {
+        throw new ApiError(401, "User must be logged in");
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete({
+        _id: commentId,
+        owner: req.user?._id
+    })
+
+    if (!deletedComment) {
+        throw new ApiError(404, "Comment not found or you're not authorized to delete it");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, deletedComment, "Comment deleted successfully")
+    )
+
 })
 
 export {
