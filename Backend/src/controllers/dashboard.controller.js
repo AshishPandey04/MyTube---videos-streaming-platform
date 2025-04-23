b4f9f74e685063682a51be3ad16e8ae323f28bca
AshishPandey04 <0203ashish2004@gmail.com>
2025-04-23 22:08:13 +0530

    Refactor dashboard controller to improve user authentication checks. Update error handling to ensure proper validation of user login before retrieving channel statistics and videos. Enhance user ID handling with mongoose ObjectId conversion.

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -8,10 +8,11 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-    const userId = req.user._id;
-    if(!userId){
+    
+    if(!req.user){
         throw new ApiError(400,"Login to get Channel stats")
     }
+    const userId= new mongoose.Types.ObjectId(req.user._id)
 
     const [totalVideos, totalSubscribers, videoIds] = await Promise.all([
       Video.countDocuments({ owner: userId }),
@@ -46,10 +47,10 @@ const getChannelStats = asyncHandler(async (req, res) => {
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
 
-    const userId = req.user._id;
-    if(!userId){
-        throw new ApiError(400,"Login to get all videos")
+    if(!req.user){
+        throw new ApiError(400,"Login to get all channel videos")
     }
+    const userId= new mongoose.Types.ObjectId(req.user._id)
 
     const videos = await Video.find({
       owner: userId,
